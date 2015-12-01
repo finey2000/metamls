6914d878d160b3262b2b58f3518752ba43a2cf3f
Christian Ntong <finey2000@gmail.com>
2015-12-01 01:04:52 -0800

    fixed user validation error for production testing

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   validates :username, length: {minimum:2}, uniqueness: true, presence: true
   validates :password, length: {minimum:6}, presence: true
   validates :email, uniqueness: true, presence: true
-  validates :firstname, allow_blank: true
-  validates :surname, allow_blank: true
-  validates :token, allow_blank: true
-  validates :timeout, allow_blank: true
+  validates :firstname, presence: true, allow_blank: true
+  validates :surname, presence: true, allow_blank: true
+  validates :token, presence: true, allow_blank: true
+  validates :timeout, presence: true, allow_blank: true
 end
