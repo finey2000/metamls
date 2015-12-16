8000593fc94bc35a79134a3bb61ef872b3cc72a3
Christian Ntong <finey2000@gmail.com>
2015-12-16 08:33:37 -0800

    Made pagination a little more decent

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,32 +12,43 @@ class HomeController < ApplicationController
     req_page = params[:pg].to_i
     current_page = (req_page >= 1) ? (req_page - 1) : 0
     offset =  per_page * current_page
-    calculate_pagination(current_page+1,per_page,Property.count)
+    pages = (Property.count/per_page).ceil
+    calculate_pagination(current_page+1,pages)
     @properties = Property.order(id: :desc).limit(per_page).offset(offset)
   end
   
-  def calculate_pagination(current_page,per_page,total)
-     pages = (total/per_page).ceil
-     next_count = prev_count = 3 #how many links to display on page
+  def calculate_pagination(current_page,pages)
+     first_page = 1     
+     next_count = prev_count = 4 #how many links to display on page  
+     next_page = current_page == pages ? pages : current_page+1     
+     prev_page = current_page == first_page ? first_page : current_page-1     
      @pagination = {}
      @pagination[:current_page] = current_page     
-     @pagination[:prev_page] = (current_page - 1) unless current_page == 1
-     @pagination[:next_page] = (current_page + 1) unless current_page == pages     
+     @pagination[:prev_page] = prev_page
+     @pagination[:next_page] = next_page
      @pagination[:last_page] = pages
-     @pagination[:first_page] = 1
+     @pagination[:first_page] = first_page
      @pagination[:prev_pages] = []
      @pagination[:next_pages] = []
-     if current_page > prev_count
-          for prevc in ((current_page-prev_count)..(current_page-1))
-            @pagination[:prev_pages].push(prevc)
-          end
-     end
-     
-    if pages > (current_page+next_count)
-              for nextc in ((current_page+1)..(current_page+next_count))
-                @pagination[:next_pages].push(nextc)
+#    Calculate next page numbers
+    if current_page < pages
+              while next_page
+                @pagination[:next_pages].push(next_page)
+                next_page += 1
+                next_page = false if next_page > pages
+                next_page = false if next_page == (current_page + next_count)                
               end
-    end
+    end    
+#    Calculate previous page numbers
+    if current_page > first_page
+              while prev_page
+                @pagination[:prev_pages].push(prev_page)
+                prev_page -= 1
+                prev_page = false if prev_page <= 1
+                prev_page = false if prev_page == (current_page - prev_count)                
+              end
+              @pagination[:prev_pages].reverse!
+    end    
 
   end
   
